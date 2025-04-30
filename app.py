eb1a4298241c92f9483995062a601fab231c134f
Cyrus <cyruswambugu91@gmail.com>
2025-04-30 18:07:51 +0300

    ammending .py file host to 0.0.0.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # %%
 from flask import Flask, request, render_template_string
 import threading
+import os
 
 app = Flask(__name__)
 
@@ -43,7 +44,8 @@ def home():
 
 
 def run_app():
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 threading.Thread(target=run_app).start()
 
